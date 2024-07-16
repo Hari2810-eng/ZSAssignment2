ac4a9d357638e681be0732494b267351f64b8032
Hari2810-eng <116560276+Hari2810-eng@users.noreply.github.com>
2024-07-16 12:00:00 +0530

    Update Program3.java

--- Exam_1/Program3.java
+++ Exam_1/Program3.java
@@ -1,43 +1,44 @@
-import java.util.*;
-
 public class Program3 {
 
     static int findGrandchildren(String[][] childFather, String grandpa) {
-        int count=0;
-        Map<String, Set<String>> map = new HashMap<>();
-        for(int i=0; i<childFather.length; i++){
-            if(!map.containsKey(childFather[i][1])){
-                map.put(childFather[i][1], new HashSet<>());
-                
-            }
-            map.get(childFather[i][1]).add(childFather[i][0]);
-        }
-        Set<String> fathers = map.get(grandpa);
-        if(fathers == null){
-            return count;
-        }
-        for(String father:fathers){
-            if(map.get(father)!=null){
-                count+=map.get(father).size();
+        int count = 0;
+
+        String[] childrenOfGrandpa = new String[childFather.length];
+        int childrenCount = 0;
+        
+        for (int i = 0; i < childFather.length; i++) {
+            if (childFather[i][1].equals(grandpa)) {
+                childrenOfGrandpa[childrenCount] = childFather[i][0];
+                childrenCount++;
             }
         }
+        
+        for (int i = 0; i < childrenCount; i++) {
+            String child = childrenOfGrandpa[i];
+            
+            for (int j = 0; j < childFather.length; j++) {
+                if (childFather[j][1].equals(child)) {
+                    count++;
+                }
+            }
+        }
+
         return count;
     }
 
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        String[][] input = new String[][]{{"Paari", "Karikalan"},
-                                        {"Udhiran", "Sendhan"},
-                                        {"Muthalvar", "Paari"},
-                                        {"Sendhan", "Arulmozhi"},
-                                        {"Karikalan", "Sendhan"},
-                                        {"Stalin", "Paari"},
-                                        {"Kalangarai", "Paari"}
-                                        };
+        // Test data
+        String[][] input = new String[][]{
+            {"Paari", "Karikalan"},
+            {"Udhiran", "Sendhan"},
+            {"Muthalvar", "Paari"},
+            {"Sendhan", "Arulmozhi"},
+            {"Karikalan", "Sendhan"},
+            {"Stalin", "Paari"},
+            {"Kalangarai", "Paari"}
+        };
     
         String grandpa = "Karikalan";
-        System.out.println(findGrandchildren(input, grandpa));
-                
-        
+        System.out.println("Number of grandchildren: " + findGrandchildren(input, grandpa));
     }
 }
